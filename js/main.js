f079c7881c022ca2bbfb6d30dae5b3c66176fac7
Evgeniya <kabanovae30@gmail.com>
2021-02-07 17:17:10 +0300

    Исправление по комментариям

--- js/main.js
+++ js/main.js
@@ -2,21 +2,53 @@
 
 /* Переменные */
 
-const TITLE = ['Рауталахти Вилладж', 'Парк-Отель Хутор Ярви', 'Piter Inn', 'Prospekt'];
-const TYPE = ['palace', 'flat', 'house', 'bungalow'];
-const CHECKIN_HOUR = ['12:00', '13:00', '14:00'];
-const CHECKOUT_HOUR = ['12:00', '13:00', '14:00'];
-const FEAUTERS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const DESCRIPTION = ['Апартаменты «Проспект» с бесплатным Wi-Fi расположены в Петрозаводске, в Карелии.', 'В парк-отеле «Хутор Ярви» к услугам гостей отдельный пляж и бесплатный Wi-Fi.', 'Piter Inn расположен в центре Петрозаводска, всего в 2 минутах ходьбы от центрального железнодорожного вокзала.', 'Дом для отпуска «Рауталахти Вилладж» с собственной бесплатной парковкой расположен в деревне Рауталахти Республики Карелия.']
-const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-const ANNOUNCEMENT_QUANTITY = 10;
+const TITLE = [
+  'Рауталахти Вилладж',
+  'Парк-Отель Хутор Ярви',
+  'Piter Inn',
+  'Prospekt',
+];
+const TYPE = ['palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+const CHECKIN_HOUR = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const CHECKOUT_HOUR = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+const DESCRIPTION = [
+  'Апартаменты «Проспект» с бесплатным Wi-Fi расположены в Петрозаводске, в Карелии.',
+  'В парк-отеле «Хутор Ярви» к услугам гостей отдельный пляж и бесплатный Wi-Fi.',
+  'Piter Inn расположен в центре Петрозаводска, всего в 2 минутах ходьбы от центрального железнодорожного вокзала.',
+  'Дом для отпуска «Рауталахти Вилладж» с собственной бесплатной парковкой расположен в деревне Рауталахти Республики Карелия.',
+];
+const PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
 
 /*Генерация чисел*/
 
 const generateRandomInt = (min, max) => {
   if (max > min && min >= 0) {
-    const result = Math.floor (Math.random () * (max - min + 1) + min);
-    return result >= min ? result : generateRandomInt (min, max);
+    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    return result >= min ? result : generateRandomInt(min, max);
   }
 
   throw new Error ('Ошибка входных данных');
@@ -26,8 +58,8 @@ const generateRandomInt = (min, max) => {
 
 const getRandomFloat = (min, max, decimal = 2) => {
   if (max > min && min >= 0) {
-    const result = (Math.random () * max).toFixed (decimal);
-    return result >= min ? result : getRandomFloat (min, max, decimal);
+    const result = (Math.random() * max).toFixed(decimal);
+    return result >= min ? result : getRandomFloat(min, max, decimal);
   }
 
   throw new Error ('Ошибка входных данных');
@@ -35,17 +67,17 @@ const getRandomFloat = (min, max, decimal = 2) => {
 
 /*Генерация элемента массива*/
 
-let getRandomArrayElement = function (elements) {
-  return elements[generateRandomInt (0, elements.length - 1)];
+const getRandomArrayElement = function (elements) {
+  return elements[generateRandomInt(0, elements.length - 1)];
 };
 
 /*Генерация случайной длины массива*/
 
-const getUniqueArray = (elements) => {
+const getUniqueArray = elements => {
   let uniqueArray = [];
-  for (let i=0; i <= generateRandomInt(0, elements.length-1); i++){
-    let uniqueIndex = generateRandomInt(0, elements.length-1);
-    if (uniqueArray.indexOf(elements[uniqueIndex]) == -1){
+  for (let i = 0; i <= generateRandomInt(0, elements.length - 1); i++) {
+    const uniqueIndex = generateRandomInt(0, elements.length - 1);
+    if (uniqueArray.indexOf(elements[uniqueIndex]) === -1) {
       uniqueArray.push(elements[uniqueIndex]);
     }
   }
@@ -55,42 +87,45 @@ const getUniqueArray = (elements) => {
 /*Описание функций для массива*/
 
 const getAuthor = () => {
-  return  {avatar: 'img/avatars/user' + 0 + generateRandomInt(1, 8) + '.png'};
+  const minNumberImage = 1;
+  const maxNumberImage = 8;
+  return {
+    avatar: `img/avatars/user0${generateRandomInt(minNumberImage, maxNumberImage)}.png`,
+  };
 };
 
 const getLocation = () => {
+  const minCoordinateX = 35.65000;
+  const maxCoordinateX = 35.70000;
+  const minCoordinateY = 139.70000;
+  const maxCoordinateY = 139.80000;
+  const decimalForLocation = 5;
   return {
-    x: getRandomFloat(35.65000, 35.70000, 5),
-    y: getRandomFloat(139.70000, 139.80000, 5),
+    x: getRandomFloat(minCoordinateX, maxCoordinateX, decimalForLocation),
+    y: getRandomFloat(minCoordinateY, maxCoordinateY, decimalForLocation),
   };
 };
 
-const getOffer = () => {
-  return {
-    title: getRandomArrayElement (TITLE),
-    address:  Object.values(getLocation()),
-    price: generateRandomInt(0, 1000000),
-    type: getRandomArrayElement (TYPE),
-    rooms: generateRandomInt(0, 100),
-    guests: generateRandomInt(0, 100),
-    checkin: getRandomArrayElement (CHECKIN_HOUR),
-    checkout: getRandomArrayElement (CHECKOUT_HOUR),
-    features: getUniqueArray (FEAUTERS),
-    description: getRandomArrayElement (DESCRIPTION),
-    photos: getUniqueArray (PHOTOS),
-  };
-};
+const generateOffer = () => ({
+  title: getRandomArrayElement(TITLE),
+  address: Object.values(getLocation()),
+  price: generateRandomInt(0, 1000000),
+  type: getRandomArrayElement(TYPE),
+  rooms: generateRandomInt(0, 100),
+  guests: generateRandomInt(0, 100),
+  checkin: getRandomArrayElement(CHECKIN_HOUR),
+  checkout: getRandomArrayElement(CHECKOUT_HOUR),
+  features: getUniqueArray(FEATURES),
+  description: getRandomArrayElement(DESCRIPTION),
+  photos: getUniqueArray(PHOTOS),
+});
 
-const getTotalObject = () => {
-  return {
-    author: getAuthor(),
-    offer: getOffer(),
-    location: getLocation(),
-  };
-};
+const getTotalObject = () => ({
+  author: getAuthor(),
+  offer: generateOffer(),
+  location: getLocation(),
+});
 
-const createSeveralAnnouncements = () => {
-  return new Array(ANNOUNCEMENT_QUANTITY).fill(null).map(getTotalObject);
-};
+const createAnnouncements = new Array(10).fill(null).map(() => getTotalObject());
 
-createSeveralAnnouncements();
+createAnnouncements;
